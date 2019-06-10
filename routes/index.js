7b5928d808ea93ea3ea5c2464a1359b0990f4ab6
MSeoJun <mun1582w@naver.com>
2019-06-11 00:37:02 +0900

    라우터 역할

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,13 @@ const pool = mysql.createPool({
   user            : 'root',
   password        : 'noo1128148',
   database        : 'irving',
-  dateStrings     : 'date'
+  dateStrings     : 'date' 
 });
 
 /*회원정보 페이지로*/
 router.get('/index', function(req, res, next) {
 
-  pool.getConnection(function(err, conn){
+  pool.getConnection(function(err, conn){ 
     conn.query('SELECT * FROM user;', function(err, results){
       res.render('index', { results: results });
       
@@ -58,15 +58,43 @@ router.get('/logout', function(req, res, next) {
 });
 
 /*회원가입 페이지로 이동*/
-// router.get('/register', function(req, res, next) {
+router.get('/register', function(req, res, next) {
 
-//   pool.getConnection(function(err, conn){
-//     conn.query('SELECT * FROM user;', function(err, results){
-//       res.render('sign_up', { results: results });
+  pool.getConnection(function(err, conn){
+    conn.query('SELECT * FROM user;', function(err, results){
+      res.render('sign_up', { results: results });
       
-//       conn.release();
-//     });
-//   });
+      conn.release();
+    });
+  });
+});
+
+// 회원가입에서 적은 정보를 저장
+router.post('/sign_up', function(req, res, next) {
+  const name = req.body.name;
+  const age = req.body.age;
+  const birth = req.body.birth;
+  const hobby = req.body.hobby;
+  const phone = req.body.phone;
+  const email = req.body.email;
+  const pw = req.body.pw;
+  pool.getConnection(function(err, conn){
+    conn.query(`SELECT * FROM user WHERE email = '${email}'`,function(err, result){
+      if(result.length > 0) {
+        const error = 100;
+        res.render('sign_up', {ERROR: error});
+      } else {
+        conn.query(`INSERT INTO user(name, age, gender, birth, hobby, phone, email, pw)VALUES('${name}', '${age}', '${gender}', '${birth}', '${hobby}', '${phone}', '${email}', MD5('${pw}'));`,function(err, result){
+          conn.query(`SELECT * FROM user WHERE name = '${name}' AND age = '${age}' AND birth = '${birth}' AND hobby = '${hobby}' AND phone = '${phone}' AND email = '${email}' AND pw = MD5('${pw}');`,function(err, result){
+          const error = 0;
+          const success = 100;
+          res.render('login', {ERROR: error, SUCCESS: success});
+        });
+      });
+    });
+  });
+});
+
 
 /*아이디 비밀번호 체크*/
 router.post('/Login', function(req, res, next) {
@@ -90,4 +118,3 @@ router.post('/Login', function(req, res, next) {
 });
 
 module.exports = router;
- 
