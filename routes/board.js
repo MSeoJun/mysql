a9bef62d7fc532f7949c031b6edc4b6e82bf4f17
MSeoJun <mun1582w@naver.com>
2019-06-26 09:18:04 +0900

    게시판 라우터

--- routes/board.js
+++ routes/board.js
@@ -7,7 +7,7 @@ const pool = require('../config/dbconfig');
 router.get('/notice', function(req, res, next) {
 
   pool.getConnection(function(err, conn){
-    conn.query('SELECT * FROM notice;', function(err, results){
+    conn.query(`SELECT b.*,(SELECT count(*) from comment WHERE notice_id=b.id) as 'count' FROM notice as b;`, function(err, results){
       res.render('notice/notice', { results: results });
       
       conn.release();
@@ -45,6 +45,8 @@ router.post('/create2', function(req, res, next) {
 
 /*게시판글 삭제*/
 router.get('/node', function(req, res, next) {
+  console.log(req.query.name);
+  
   if(req.query.name != req.session.ID){
     res.render('notice/error');
   } else{
@@ -62,12 +64,12 @@ router.get('/node', function(req, res, next) {
 // 게시글 수정하는 페이지로 이동
 
 router.get('/update', function(req, res, next) {
-  console.log('/board/update');
   const title = req.query.title;
   const content = req.query.content;
-  console.log(req.query.name);
+  const id = req.query.id;
+  const name = req.query.name;
 
-  // console.log(req.query.id);
+  console.log(req.query.name);
   
   if(req.query.name != req.session.ID){
     res.render('notice/error');
@@ -96,26 +98,39 @@ router.get('/update', function(req, res, next) {
   pool.getConnection(function(err, conn){
     conn.query(`UPDATE notice SET title = '${title}', content = '${content}' WHERE id ='${reqid}';`,function(err, results){
       res.redirect('/board/notice');
- 
- 
+  
  
       conn.release();
     });
   });
  });
 
-/*게시판글 등록으로 이동*/
+/*게시판 상세보기로 이동*/
 router.get('/tkd', function(req, res, next) {
-  res.render('notice/detail');
-  return;
-  console.log('111');
+  const id = req.query.id;
     pool.getConnection(function(err, conn){
       conn.query(`SELECT * FROM notice WHERE id='${req.query.id}';`, function(err, results){
-        res.render('notice/detail', { results: results });
+        conn.query(`SELECT * FROM comment WHERE notice_id = ${id};`,function(err, com){
+          res.render('notice/detail', { results: results , com: com , id:id});
+        });
+        
         
         conn.release();
       });
     });
   });
+
+/*댓글 등록으로 이동*/
+router.post('/comment', function(req, res, next) {
+  const comment = req.body.content;
+  const id = req.body.id;
+  pool.getConnection(function(err, conn){
+    conn.query(`INSERT INTO comment (notice_id,email,content) VALUES ('${id}','${req.session.ID}','${comment}')`, function(err, results){
+      res.redirect(`/board/tkd?id=${id}&email=${req.session.ID}&comment=${comment}`);
+      
+      conn.release();
+    });
+  });
+});
 
 module.exports = router;
